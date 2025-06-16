dccdf0b6a98dd6c8a8910aa1b12dafe121ebedbb
Chupacabriks <mufunbetsuna@gmail.com>
2025-06-16 12:46:19 +0300

    add smooth scroll

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,13 +5,33 @@ const menu = () => {
   const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
 
+  const scrollBtn = document.querySelector('a[href="#service-block"]');
+
+  //функция зарытия меню 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
+  const scroll = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href'); //узнаем куда скроллить
+    if (targetId !== '#') {
+      const targetE = document.querySelector(targetId);
+      if (targetE) {
+        targetE.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      }
+    }
+    if (e.currentTarget.closest('menu')) {
+      handleMenu();
+    }
+  };
+
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
-
-  menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+  scrollBtn.addEventListener('click', scroll)
+  menuItems.forEach(menuItem => menuItem.addEventListener('click', scroll));
 };
 export default menu
