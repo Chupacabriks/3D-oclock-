3c14478a4d1c0b2fe49fe338a085c6882cf2edb5
Chupacabriks <mufunbetsuna@gmail.com>
2025-06-21 21:17:39 +0300

    add:  check for inputs

--- src/modules/input.js
+++ src/modules/input.js
@@ -3,11 +3,93 @@ const input = () => {
 
   const calcBlock = document.querySelector('.calc-block');
   const input = calcBlock.querySelectorAll('input[type="text"]');
+  const inputsTextMes = document.querySelectorAll('input[type="text"], input[placeholder="Ваше сообщение"]');
+  const inputEmail = document.querySelectorAll('input[type="email"]');
+  const inputTel = document.querySelectorAll('input[type="tel"]');
+
+
+  const isCyrillic = /^[а-яёА-ЯЁ\s-]$/;
+  const isPhone = /^[0-9()\-\s]+$/;
+  const isEmail = /^[a-zA-Z0-9@._%+\-!~*']$/;
+  // const isEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+  const isControlKey = (key, e) => {
+    return (key === 'Backspace' || key === 'Delete' ||
+      key === 'ArrowLeft' || key === 'ArrowRight' ||
+      key === 'Tab' || key === 'Enter' || key === 'Shift' || key === 'CapsLock' ||
+      e.ctrlKey || e.metaKey)
+  }
+
+
+
   input.forEach((inputElement) => {
     inputElement.addEventListener('input', (e) => {
       e.target.value = e.target.value.replace(/\D/g, '');
     });
   })
+
+
+
+
+  inputsTextMes.forEach(input => {
+    input.addEventListener('keydown', e => {
+      const key = e.key;
+      if (isControlKey(key, e)
+      ) return;
+
+      if (!isCyrillic.test(key)) {
+        e.preventDefault();
+        alert('Недопустимые символы. Разрешены только кириллица, пробел и тире.');
+      }
+    });
+  });
+
+
+  inputTel.forEach(input => {
+    input.addEventListener('keydown', e => {
+      const key = e.key;
+
+      if (isControlKey(key, e)
+      ) return;
+
+      if (!isPhone.test(key)) {
+        e.preventDefault();
+        alert('Недопустимые символы. Разрешены только цифры, круглые скобки, дефис');
+      }
+    });
+  });
+
+  inputEmail.forEach(input => {
+    input.addEventListener('keydown', e => {
+      const key = e.key;
+
+      if (isControlKey(key, e)
+      ) return;
+
+      if (!isEmail.test(key)) {
+        e.preventDefault();
+        alert('Недопустимые символы. Введите корректный формат почты на латинице');
+      }
+    });
+  });
+
+
+  // inputEmail.forEach(input => {
+  //   let timer;
+  //   input.addEventListener('input', () => {
+  //     clearTimeout(timer);
+
+  //     timer = setTimeout(() => {
+  //       const value = input.value.trim();
+  //       if (value === '') return;
+  //       if (!isEmail.test(value)) {
+  //         alert('Введите корректный email. Пример: name@email.com');
+  //       }
+  //     }, 800);
+  //   });
+  // });
+
 
 }
 
