572f4d0370702deb3edd9f1e1c8c949710476892
Chupacabriks <mufunbetsuna@gmail.com>
2025-06-27 15:40:32 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import input from './modules/input';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('29 june 2025');
 menu();
 modal();
 input();
 tabs();
+slider();
